8d8dc30f6aee7b1ae775f05b9ab797692fdd7780
Lucas Axberg <lucas.axberg@gmail.com>
2024-01-09 15:29:11 +0100

    Added option to list all names of account for selecting wich to display later

--- src/GetData.py
+++ src/GetData.py
@@ -1,8 +1,9 @@
 # Lucas Axberg | GetData
 # Reads data from file/database
+
 import json
 
-def getAccountFromFile(account: str, user: str):
+def getAccountFromFile(user, account=""):
     """
     Returns the information about the account for the specified user.
     If the account doesn't exist, it returns False.
@@ -12,17 +13,24 @@ def getAccountFromFile(account: str, user: str):
     with open("./GetData.json", "r") as file:
         data = json.loads(file.read())
 
+    # Defines a list for names in case of no account specified
+    account_names = []
+    
     # Saves the list of dictionaries
     user_accounts = data["users"][user]
 
     # Saves formatted_data as the specified account if it exists, else sets it to False
     for account_data in user_accounts:
-        if account_data["account"] == account:
+        if account == "":
+            account_names.append(account_data["account"])
+        elif account_data["account"] == account:
             formatted_data = account_data
             break
     else: 
         formatted_data = False
         
-    # Returns datastructure or False
-    return formatted_data
-    
+    # Returns datastructure or list of names if account is not specified
+    if account == "":
+        return account_names
+    else:
+        return formatted_data
