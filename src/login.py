2905626207dc6b2eb601b019efc8cc8105f6f495
abbgavger <gaverstedt.lysander.geranis@abbgymnasiet.se>
2024-01-15 14:52:29 +0100

    created signup and removed input in login function

--- src/login.py
+++ src/login.py
@@ -1,20 +1,24 @@
 # Lysander
 
+
 import json
 
+# Delete later
 def getUserList():
-    users = open("src/users.json", "r")
+    '''Opens the json file with users and returns a dictionary.'''
+
+    users = open("src/Users.json", "r")
     user_list = json.loads(users.read())
     users.close()
     return user_list
 
 
-def checkExistingUsers():
-    username_input = input("Username > ")
-    password_input = input("Password > ")
+def login(username_input, password_input):
+    '''Gets username and password input from user and checks if it exists.'''
 
     user_list = getUserList()
 
+    # If it does exist it returns True else it returns False.
     try:
         if user_list["correct"][username_input] == password_input:
             return True
@@ -22,3 +26,10 @@ def checkExistingUsers():
             return False
     except KeyError:
         return False
+
+
+username_input = input("Username > ")
+password_input = input("Password > ")
+
+test = login(username_input, password_input)
+print(test)
