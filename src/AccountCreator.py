9df3ce4b1f7e4a0f8df021be06a7bd725cb15eef
ABB22denfor <drexman45@gmail.com>
2024-01-16 15:39:26 +0100

    Fixed bugs, 99% done

--- src/AccountCreator.py
+++ src/AccountCreator.py
@@ -1,13 +1,12 @@
 '''Module for adding a new user account'''
 import sys
+import os
 #Remove main and sys when done!
 def main():
     boolean = strToBool(sys.argv[1].strip())
-    newAccount = createAccount(boolean)
-    printAccount(newAccount)
+    print(createAccount(boolean))
 
-#Remove when done
-def strToBool(inputStr):
+def strToBool(inputStr: str) -> bool:
     return bool(inputStr.lower() == "true" or inputStr.lower() == "y")
 
 def printUI():
@@ -18,19 +17,19 @@ WELCOME TO ACCOUNT CREATOR 3000!
 (*required)
 ''')
 
-def createAccount(wantsUserPassword: bool):
-    '''(wantsUserPassword->bool) Create a new account with name, email and password'''
+def createAccount(wantsUserPassword: bool) -> dict:
+    '''(wantsUserPassword: bool) -> dict, Create a new account with name, email and password'''
 
     printUI()
 
     password = ""
 
     #What is the account for? (e.g. Youtube, Spotify, Facebook)
-    accountType = input("*Account for: \n > ")
+    accountType = input("*Application / Website: \n > ")
 
     name = input("*Username: \n > ")
 
-    email = input("Email address: \n > ")
+    email = input(" Email address: \n > ")
 
     if wantsUserPassword:
         password = input("*Password: \n > ")
@@ -38,24 +37,27 @@ def createAccount(wantsUserPassword: bool):
         #Call createPassword with functionHandler
         pass
 
-    return {"username": name, "email": email, "password": password, "account": accountType}
+    return printAccount({"username": name, "password": password, "email": email, "account": accountType})
 
-def printAccount(account):
+
+def printAccount(account: dict) -> dict:
+    "(account: dict) -> dict, Print account details and ask user if they want to save that account"
     print(f'''
----------------------------------
+--------------------------------
 Account Details:
 - Username: {account["username"]}
 - Email: {account["email"]}
 - Password: {account["password"]}
 - For: {account["account"]}
----------------------------------''')
+--------------------------------''')
 
     if not strToBool(input("Do you want to save this account? (y/n) \n > ")):
+        os.system('cls' if os.name == 'nt' else 'clear')
         #Ask if want to generate password or make themselves
         #Change parameter to corresponding input
-        createAccount(True)
-    else:
-        print(account)
+        return createAccount(True)
+
+    return account
 
 
 if __name__ == "__main__":
