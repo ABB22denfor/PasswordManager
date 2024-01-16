df1bf1e702cdee84fe74a01c6b59b7f158ebb5a8
Lucas Axberg <lucas.axberg@gmail.com>
2024-01-16 16:03:53 +0100

    Added function for changing data of account

---
+++ src/EditPrompt.py
@@ -0,0 +1,38 @@
+# Author: Lucas Axberg
+
+import os
+from GetData import getAccountFromFile
+
+
+def editPrompt(account):
+
+    # Stores the account dictionary to a variable
+    acc = getAccountFromFile(account)
+
+    while True:
+
+        os.system("cls" if os.name == "nt" else "clear")
+
+        # Prints the account information
+        for key in acc:
+            print(f"{key.title()}: {acc[key]}")
+        print("---------------------")
+
+        # Takes user input and formats it
+        print("Select a field to edit:")    
+        field_to_change = input("Edit > ").strip().lower()
+
+        # Breaks loop if input is correct
+        if field_to_change in acc.keys():
+            break
+
+        # Returne false if user wants to exit
+        if field_to_change == "exit":
+            return False
+        
+
+    print("---------------------")
+    new_value = input(f"Change {field_to_change} to: ")
+
+    acc[field_to_change] = new_value
+    return acc
