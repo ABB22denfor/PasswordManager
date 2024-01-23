f5ce6f3f9fda7a28e104e83a53a4a57dbd9350a3
ABB22denfor <drexman45@gmail.com>
2024-01-23 13:19:23 +0100

    Made code more readable and added small error handling

--- src/EditSavedValues.py
+++ src/EditSavedValues.py
@@ -1,25 +1,19 @@
 #Dennis
 '''Module for saving edited data'''
-import os, json
+import os
+import json
 from DataCopy import copyDataToTemp
 
-account = {
-    "username": "Bob",
-    "password": "5697605",
-    "account": "Youtube"
-}
-
-def main():
-    editSavedValue("Bob", account, ["password", "354354353"])
-
-def editSavedValue(account_name, account_data, edited_value: list):
-    '''(editedValue: list [Val to Edit, New Val]) Write edited data to UserData.json and update temp user'''
+def editSavedValue(account_name: str, account_data: dict, edited_value: list):
+    '''(account_name: str, account_data: dict, edited_value: list [Val to Edit, New Val])
+    Write edited data to UserData.json and update CurrentUser.json
+    '''
     val_to_edit = edited_value[0]
     new_val = edited_value[1]
 
     all_loaded_data = {}
-    loaded_user_data = {}
     account_to_change = {}
+    account_index = 0
 
     folder_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -27,18 +21,22 @@ def editSavedValue(account_name, account_data, edited_value: list):
     with open(f"{folder_path}/UserData.json", "r", encoding="utf-8") as f:
         all_loaded_data = json.loads(f.read())
         for saved_account in all_loaded_data["users"][f"{account_name}"]:
-            if saved_account["account"] == account_data["account"]:
+            # Check if passed in account data matches saved data
+            try:
+                if saved_account["account"] != account_data["account"]:
+                    continue
                 account_to_change = saved_account
-                loaded_data = all_loaded_data["users"][f"{account_name}"][all_loaded_data["users"][f"{account_name}"].index(saved_account)]
- 
-    loaded_data[f"{val_to_edit}"] = new_val
-    all_loaded_data["users"][f"{account_name}"][all_loaded_data["users"][f"{account_name}"].index(account_to_change)] = loaded_data
+                account_index = all_loaded_data["users"][f"{account_name}"].index(account_to_change)
+                break
+            except KeyError:
+                return print("JSONFORMATERROR")
+        else:
+            return print("ACCOUNTNOTFOUND")
+
+    account_to_change[f"{val_to_edit}"] = new_val
+    all_loaded_data["users"][f"{account_name}"][account_index] = account_to_change
 
     with open(f"{folder_path}/UserData.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(all_loaded_data))
 
-#    copyDataToTemp(account["username"])
-    # Call copyDataToTemp(account) from module DataCopy
-
-if __name__ == "__main__":
-    main()
+    copyDataToTemp(account_name)
