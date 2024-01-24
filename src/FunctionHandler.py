37d95387a3bd6bd30946f7381df662ab91a2c5bc
ABB22denfor <drexman45@gmail.com>
2024-01-24 12:49:57 +0100

    Fixes on FunctionHandler

--- src/FunctionHandler.py
+++ src/FunctionHandler.py
@@ -4,54 +4,39 @@
 #For dynamically importing modules
 import importlib
 
-#Allowed function calls
-functions = ["save", "view", "s", "v"]
-
-#Remove main later
-def main():
-    printUserInterface()
-    handleInput()
-
 def printUserInterface():
     '''Print usable commands'''
 
-    print('''
-Available Commands
+    print('''Available Commands
 ------------------
 > save new password
 > view all passwords
 ------------------''')
 
-def handleInput():
-    '''Compares user input to available modules then calls the function in chosen module'''
+def handleInput(user_input: str):
+    '''(user_input: str)
+    Compares user input to available modules then calls the function in chosen module'''
 
-    function = ""
-
-    #Retry input if no match
-    while function not in functions:
-        function = input("> ").strip().lower()
-
+    if user_input in ['save', 's']:
+        function = "askPasswordType"
+        module = "Saver"
+    else:
+        function = "chooseAccount"
+        module = "ChooseAccountToView"
     #Error-handling due to modules not fully iplemented
     try:
-        # TODO: Return function call
-        callFunctionInModule(function, function, [])
+        return callFunctionInModule(module, function, [])
     except ModuleNotFoundError:
-        print("MODULENOTFOUND")
+        return print("MODULENOTFOUND")
 
 def callFunctionInModule(module: str, function: str, args: list):
-    '''(module->str, function->str, args->list) Call function inside of module with specified args'''
+    '''(module: str, function: str, args: list)
+    Call function inside of module with specified args'''
 
     #Imports module with same name as input
     mod = importlib.import_module(module)
 
-    print("Getting module" + module + function)
-
     #Get function from string parameter
     function = getattr(mod, function)
 
-    #Call function with unspecified amount of args
-    # TODO: Return tuple of function and args
-    function(*args)
-
-if __name__ == "__main__":
-    main()
+    return (function, args)
