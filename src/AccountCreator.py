f36efe6b2a790d8d42b9f94c4438ab2cbe68fa39
ABB22denfor <drexman45@gmail.com>
2024-01-31 10:06:09 +0100

    Cleaned up AccountCreator

--- src/AccountCreator.py
+++ src/AccountCreator.py
@@ -1,30 +1,17 @@
 #Dennis
 '''Module for adding a new user account'''
-import sys
 import os
 
 from FunctionHandler import callFunctionInModule
 from Saver import askPasswordType
 
-#Remove main and sys when done!
-def main():
-    createAccount()
-
 def strToBool(input_str: str) -> bool:
+    '''(input_str: str) Convert y/N input to True or False'''
     return input_str.lower() == "y"
 
-def printUI():
-
-    print('''
-WELCOME TO ACCOUNT CREATOR 3000!
---------------------------------
-(*required)
-''')
 
 def createAccount() -> dict:
-    '''(wantsUserPassword: bool) -> dict, Create a new account with name, email and password'''
-
-    printUI()
+    '''Create a new account with name, email and password'''
 
     password = ""
 
@@ -47,8 +34,7 @@ def createAccount() -> dict:
 
 def printAccount(account: dict) -> dict:
     "(account: dict) -> dict, Print account details and ask user if they want to save that account"
-    print(f'''
---------------------------------
+    print(f'''--------------------------------
 Account Details:
 - Username: {account["username"]}
 - Email: {account["email"]}
@@ -58,12 +44,6 @@ Account Details:
 
     if not strToBool(input("Do you want to save this account? (y/n) \n > ")):
         os.system('cls' if os.name == 'nt' else 'clear')
-        #Ask if want to generate password or make themselves
-        #Change parameter to corresponding input
         return createAccount()
 
     return account
-
-
-if __name__ == "__main__":
-    main()
