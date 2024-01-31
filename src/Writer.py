017608d6c31fbb418b862e057c5ff110781c4a08
Lucas Axberg <lucas.axberg@gmail.com>
2024-01-31 13:37:28 +0100

    Removed unnecessary error handling, and returning

--- src/Writer.py
+++ src/Writer.py
@@ -5,7 +5,7 @@ from ReadData import readFile
 from WriteData import writeFile
 
 
-def writeData(new_data: dict, user: str) -> bool:
+def writeData(new_data: dict, user: str):
     '''Updates the current data with the new data and returns False if the user does not exist else it returns True'''
 
     # Adds the new data to the current data
@@ -13,14 +13,9 @@ def writeData(new_data: dict, user: str) -> bool:
     current_data_list.append(new_data)
     
     # Tries to replace the old data with the new current data in the UserData.json file.
-    try:
-        user_data_list = readFile("UserData.json")
-        user_data_list["users"][user] = current_data_list
+    user_data_list = readFile("UserData.json")
+    user_data_list["users"][user] = current_data_list
 
-        writeFile("UserData.json", user_data_list)
+    writeFile("UserData.json", user_data_list)
 
-        copyDataToTemp(user)
-    except KeyError:
-        return False
-    
-    return True
+    copyDataToTemp(user)
