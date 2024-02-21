912ca0772f864d8c07ac09f8b1f2af5e49c9c23e
ABB22denfor <drexman45@gmail.com>
2024-02-21 10:58:23 +0100

    Added viewAccount function

--- src/ArgsHandler.py
+++ src/ArgsHandler.py
@@ -1,17 +1,24 @@
 from HandleDefaultUser import getDefaultUser, setDefaultUser
+from FunctionHandler import callFunctionInModule
+from GetData import getAccountFromFile
+from PrintInfo import printAccountInfo
+from DataCopy import copyDataToTemp
+from ReadData import readFile
 
 def handleArgs(args: list = []):
     
     try:
         if args[1] == "-u":
             pass
-        default_user = getDefaultUser()
+        user_info = getDefaultUser()
     except KeyError:
-        print("ERROR: Default User Not Set. passman -u <username> <password> to set defaults")
+        return print("ERROR: Default User Not Set. passman -u <username> <password> to set defaults")
+
+    copyDataToTemp(user_info[0])
 
     match args[1]:
         case "-v":
-            print("View")
+            viewAccount(args[2:])
         case "-c":
             print("Create")
         case "-e":
@@ -24,3 +31,13 @@ def handleArgs(args: list = []):
             return 1
     return 0
 
+def viewAccount(args: list):
+    data = readFile("CurrentUser.json")
+    chosen_account = args[0]
+
+    if chosen_account not in getAccountFromFile():
+        print(f"Account '{chosen_account}' doesn't exist.")
+        return
+    for account in data:
+        if chosen_account == account["account"]:
+            printAccountInfo(account)
