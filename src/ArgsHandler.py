3e2851272774e2243e51d112d98a41c7caa4a18d
ABB22denfor <drexman45@gmail.com>
2024-02-21 12:13:33 +0100

    Added function for editing account values

--- src/ArgsHandler.py
+++ src/ArgsHandler.py
@@ -1,11 +1,13 @@
+import json
+import os
+
 from HandleDefaultUser import getDefaultUser, setDefaultUser
-from FunctionHandler import callFunctionInModule
 from GetData import getAccountFromFile
 from PrintInfo import printAccountInfo
 from DataCopy import copyDataToTemp
 from ReadData import readFile
 
-def handleArgs(args: list = []):
+def handleArgs(args: list = []) -> int:
     
     try:
         if args[1] == "-u":
@@ -19,10 +21,8 @@ def handleArgs(args: list = []):
     match args[1]:
         case "-v":
             viewAccount(args[2:])
-        case "-c":
-            print("Create")
         case "-e":
-            print("Edit")
+            editAccount([user_info, args[2:]])
         case "-d":
             print("Delete")
         case "-u":
@@ -38,6 +38,52 @@ def viewAccount(args: list):
     if chosen_account not in getAccountFromFile():
         print(f"Account '{chosen_account}' doesn't exist.")
         return
+
     for account in data:
         if chosen_account == account["account"]:
             printAccountInfo(account)
+
+def editAccount(args: list):
+    data = readFile("CurrentUser.json")
+
+    for account in data:
+        if args[1][0] == account["account"]:
+            account_data = account
+        
+    account_name = args[0][0]
+
+    field_to_change = input("Edit > ").strip().lower()
+    new_value = input(f"Change {field_to_change} to: ")
+
+    all_loaded_data = {}
+    account_to_change = {}
+    account_index = 0
+
+    folder_path = os.path.dirname(os.path.abspath(__file__))
+
+    # Fetch data from UserData
+    with open(f"{folder_path}/../data/UserData.json", "r", encoding="utf-8") as f:
+        all_loaded_data = json.loads(f.read())
+        for saved_account in all_loaded_data["users"][f"{account_name}"]:
+            # Check if passed in account data matches saved data
+            try:
+                if saved_account["account"] != account_data["account"]:
+                    continue
+                account_to_change = saved_account
+                account_index = all_loaded_data["users"][f"{account_name}"].index(account_to_change)
+                break
+            except KeyError:
+                return print("JSONFORMATERROR")
+        else:
+            return print("ACCOUNTNOTFOUND")
+
+    # Update data with changes
+    account_to_change[f"{field_to_change}"] = new_value
+    all_loaded_data["users"][f"{account_name}"][account_index] = account_to_change
+
+    # Overwrite UserData
+    with open(f"{folder_path}/../data/UserData.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(all_loaded_data))
+
+    copyDataToTemp(account_name)
+
